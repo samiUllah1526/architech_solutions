03eaa53338309d233a683c9cf2873a396e7bbadc
Sami Ullah <sami.naazar1526@gmail.com>
2023-08-12 13:53:00 -0700

    add import alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { Header } from '@src/components'
 
 
 export default function App() {
   return (
+    <>
+    <Header />
     <h1 className="text-3xl font-bold underline text-red-400">
       Hello world!
     </h1>
+    </>
+
   )
 }
