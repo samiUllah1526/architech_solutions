7483123fda5fd3a64d2c9f7c86aa1dc5df2c12ac
Sami Ullah <sami.naazar1526@gmail.com>
2023-08-17 06:46:22 -0700

    adjust the stylling of images

--- src/pages/LandingPage/MarqueSection.tsx
+++ src/pages/LandingPage/MarqueSection.tsx
@@ -34,17 +34,17 @@ export const MarqueSection = () => {
             <Marquee>
                 {
                     [
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278613/galllery_7_fblnhk.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278615/galllery_9_inihf0.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278613/galllery_4_lstivu.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278615/galllery_8_dp3w2f.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278126/galllery_1_gkalsy.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278612/galllery_2_dcx3ba.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278616/galllery_3_pvgs1g.jpg"
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278613/galllery_7_fblnhk.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278615/galllery_9_inihf0.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278613/galllery_4_lstivu.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278615/galllery_8_dp3w2f.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278126/galllery_1_gkalsy.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278612/galllery_2_dcx3ba.jpg",
+                        "https://res.cloudinary.com/dxn2snoak/image/upload/w_600/v1692278616/galllery_3_pvgs1g.jpg"
                     ].map((item, index) => {
                         return (
-                            <Box key={index} width={{base: "300px", md:"500px", lg: "1000px"}} mx={5} borderRadius="lg" overflow="hidden">
-                                <Image src={item} objectFit="cover" objectPosition="center"></Image>
+                            <Box key={index} width={{base: "300px", md:"500px", lg: "700px"}} mx={5} borderRadius="lg" overflow="hidden">
+                                <Image src={item} alt={`gallery_iage_${index}`} w="100%" objectFit="cover" objectPosition="center"></Image>
                             </Box>
                         )
                     })
