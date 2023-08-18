425acf1ec546c2f796af5754f70251423cdf10e6
Sami Ullah <sami.naazar1526@gmail.com>
2023-08-18 06:50:18 -0700

    update images and fine tune images stylling

--- src/pages/LandingPage/MarqueSection.tsx
+++ src/pages/LandingPage/MarqueSection.tsx
@@ -39,7 +39,7 @@ export const MarqueSection = () => {
                         "Designing Inspirations Worldwide",
                         "Your Ambitions",
                         "Our Architectural Journey",
-                    ].map((item, index) => {
+                    ].reverse().map((item, index) => {
                         const color = index % 2 === 0 ? "primary" : "black"
                         return (
                             <Text color={color} fontSize={{ base: "24px", lg: "48px" }} fontWeight="700" mx={5}>{item}</Text>
@@ -51,24 +51,39 @@ export const MarqueSection = () => {
             <Marquee>
                 {
                     [
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278613/galllery_7_fblnhk.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278615/galllery_9_inihf0.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278613/galllery_4_lstivu.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278615/galllery_8_dp3w2f.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278126/galllery_1_gkalsy.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278612/galllery_2_dcx3ba.jpg",
-                        "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278616/galllery_3_pvgs1g.jpg"
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278613/galllery_7_fblnhk.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278615/galllery_9_inihf0.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278613/galllery_4_lstivu.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278615/galllery_8_dp3w2f.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278126/galllery_1_gkalsy.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278612/galllery_2_dcx3ba.jpg",
-                        // "https://res.cloudinary.com/dxn2snoak/image/upload/w_2000/v1692278616/galllery_3_pvgs1g.jpg"
-                    ].map((item, index) => {
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278613/galllery_7_fblnhk.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278613/galllery_7_fblnhk.jpg"
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692361614/gallery_12_cr4hiq.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692361614/gallery_12_cr4hiq.jpg",
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278613/galllery_4_lstivu.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278613/galllery_4_lstivu.jpg",
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278615/galllery_8_dp3w2f.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278615/galllery_8_dp3w2f.jpg",
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278126/galllery_1_gkalsy.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278126/galllery_1_gkalsy.jpg",
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692278612/galllery_2_dcx3ba.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692278612/galllery_2_dcx3ba.jpg",
+                        },
+                        {
+                            image: "https://res.cloudinary.com/dxn2snoak/image/upload/v1692361614/gallery_11_qulzqc.jpg",
+                            fallback: "https://res.cloudinary.com/dxn2snoak/image/upload/w_1000/v1692361614/gallery_11_qulzqc.jpg,"
+                        },
+                    ].map(({ image, fallback }, index) => {
                         return (
                             <Box key={index} width={{ base: "300px", md: "500px", lg: "700px" }} mx={5} borderRadius="lg" overflow="hidden">
-                                <Image src={item} alt={`gallery_iage_${index}`} w="100%" objectFit="cover" objectPosition="center"></Image>
+
+                                <Image src={image} alt={`gallery_iage_${index}`} w="100%" objectFit="cover" objectPosition="center"></Image>
                             </Box>
                         )
                     })
